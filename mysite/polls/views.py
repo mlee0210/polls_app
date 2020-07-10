0620dbe6689ba87815b8a1c7783dbfacb0bcbb37
Michael Lee <lee.cs.michael@gmail.com>
2020-07-10 09:37:13 -0700

    Updated html templates: removed hardcoded URLs and added namespces to account for project having multiple apps

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from .models import Question
 from django.template import loader
 
+from .models import Question
+
 # Create your views here.
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
 
 def index(request):
 	"""
@@ -26,3 +31,5 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse(f"You're voting on question {question_id}.")
+
+
