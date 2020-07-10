045c2614d780903cb185ba65f3a14bca244e176a
Michael Lee <lee.cs.michael@gmail.com>
2020-07-10 09:12:11 -0700

    Created the public interface,views, and added basic style for index.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	"""
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {'latest_question_list': latest_question_list,}
+	return HttpResponse(template.render(context, request))
+	"""
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list': latest_question_list,}
+	return render(request, 'polls/index.html', context)
+
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
