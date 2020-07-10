c240f015603e2bfc41ab68a73d42f4dd9ab12231
Michael Lee <lee.cs.michael@gmail.com>
2020-07-10 11:01:34 -0700

    Converted pollapp to use generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,26 +1,43 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect
+from django.views import generic
 from django.urls import reverse
 
 from .models import Choice, Question
 
 # Create your views here.
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+	template_name='polls/index.html'
+	context_object_name='latest_question_list'
 
-def index(request):
+	def get_queryset(self):
+
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
+
+#def detail(request, question_id):
+#	question = get_object_or_404(Question, pk=question_id)
+#	return render(request, 'polls/detail.html', {'question': question})
+
+#def index(request):
 	"""
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	template = loader.get_template('polls/index.html')
 	context = {'latest_question_list': latest_question_list,}
 	return HttpResponse(template.render(context, request))
 	"""
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list,}
-	return render(request, 'polls/index.html', context)
+#	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#	context = {'latest_question_list': latest_question_list,}
+#	return render(request, 'polls/index.html', context)
 
 
 """
@@ -28,9 +45,9 @@ def detail(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}.")
 """
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
